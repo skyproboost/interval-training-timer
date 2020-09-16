d797f025474a2886b9dceeb0d59c0cf3d8e1229d
skyproboost@mail.ru <658271Qq>
2020-09-16 19:06:47 +0300

    update

--- api/helpers/session.js
+++ api/helpers/session.js
@@ -1,13 +1,13 @@
 const expressSession = require('express-session')
 const session = expressSession({
-    // name: 'sessionId.connect',
+    name: 'sessionId.connect',
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
         sameSite: 'strict',
         httpOnly: true,
-        secure:  process.env.PROTOCOL === 'https'
+        // secure:  process.env.PROTOCOL === 'https'
     }
 })
 
@@ -16,6 +16,6 @@ module.exports = {
     destroySessionAndCookie: (req, res) => {
         req.session.destroy()
         res.cookie('CSRF_TOKEN', null, {maxAge: 0})
-        // res.cookie('sessionId.connect', null, {maxAge: 0})
+        res.cookie('sessionId.connect', null, {maxAge: 0})
     }
 }
